01f62fabf864063a2a54ad0168d71b342ecb0232
Programmer-kush <kushbohare@gmail.com>
2025-02-23 13:07:22 +0530

    sd

--- app.py
+++ app.py
@@ -6,12 +6,18 @@ import time
 # Set page configuration
 st.set_page_config(page_title="NIT Kurukshetra Info App", layout="wide")
 
-hide_github_icon = """
-GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
 # Title with animation
